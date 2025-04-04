76d11533360f2085899dc183b1e88ee01ae1c7ab
luciascott15 <lscott@mail.austral.edu.ar>
2025-04-04 11:23:14 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,7 +2,7 @@ def change():
     expense = 23.75
     money = 100
     
-    print ("Ingresar Gasto:")
+    print ("Ingresar gasto:")
     print (expense)
     print ("Dinero Recibido")
     print (money)
